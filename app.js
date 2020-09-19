2fac5b60f4193dfe5a6bdd280f119d2dc09f9844
autophile-011 <63092867+autophile-011@users.noreply.github.com>
2020-09-19 17:01:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect( {
+    parent:document.querySelector('.distortion'),
+    intensity: 1.0,
+    image1:"bw4.jpg",
+    image2:"bw3.jpeg",
+    displacementImage:"wolf Drawing.png",
+})
